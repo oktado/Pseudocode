34225980590a748ff450246871147fd6162d5ccd
Oktado Putra <oktado.putra@gmail.com>
2020-10-20 07:30:41 +0700

    menambah file tugas pseudocode

---
+++ Pseudocode.js
@@ -0,0 +1,37 @@
+// Project 1
+// 1.	Buat Variabel  Jam = 2
+// 2.	Hitung Jam*3600 didalam Var Detik 
+// 3.	Simpan hasilnya di dalam variable detik
+// 4.	Cetak hasil variable detik
+
+// Project 2
+// 1.	Buat  sebuah form input dengan nama variable NilaiSatu dengan isi “Tuliskan Nilai Pertama”
+// 2.	Buat variable  dengan nama NilaiDua di dalam form input denga nisi text “Tuliskan Nilai Pertama”
+// 3.	Buat sebuah variable dengan nama “Hasil” di pop up dengan isi text “Hasil”
+// 4.	Bandingkan antara var NilaiSatu dan NilaiDua lebih besar yang mana.
+// 5.	Simpan hasil perbandingan di var”hasil”
+// 6.	Buat alert pop up dengan menampilkan “hasil” yg berisi “NilaiSatu perbandingan NilaiDua”
+
+
+// Project 3
+// 1.	Buat variable isi jumlah traveler dengan nama “penumpang” 
+// 2.	Buat variable harga tiket pesawat dengan nama “harga”
+// 3.	Buat cekbox apakah mau membeli tiket two way
+// 4.	Buat form input  dengan nama “Jumlah Penumpang”kemudian hasilnya masukkan ke var penumpang
+// 5.	Buat form input  dengan nama “Harga Tiket” kemudian hasilnya masukkan ke var “harga”
+// 6.	Buat button A dengan nama “cek harga”
+// 7.	Buat fungsi di dalam button A, var “penumpang”*var”harga” 
+// 8.	Kemudian masukkan ke dalam variable “Hasil”.
+// 9.	Beri kondisi apabila pembeli memilih tiket two way maka “Hasil”*2.
+// 10.	Simpan “Hasil”
+// 11.	Tampilkan “Hasil” dalam Popup Alert.
+
+
+// Project 4
+// 1.	Buat Var X = 0
+// 2.	Buat Var N = nilai akhir
+// 3.	Buat form input A dengan isi text “Masukkan Angka”
+// 4.	Masukkan isi input A ke Var N
+// 5.	Kemudian jalankan fungsi untuk mencetak Var X sampai Var N.
+// 6.	Simpan Var N
+// 7.	Berikan Pop up Alert denga nisi teks Var N.
